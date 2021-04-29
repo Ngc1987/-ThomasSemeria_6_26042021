2e9e6380b9dfb0375a8159c1289a3a48db295bae
Tomss <tsemeri@gmail.com>
2021-04-29 17:46:53 +0200

    Finish lightbox and contact modal
    Fin de la mise en forme de la lightbox, ajout de la modale de contact du photographe avec accessibilité au clavier. Focus clavier sur le premier champ à l'ouverture, et avec la touche echap la modal se ferme et re-focus le bouton contact qui ouvre la modale. Le document est désactivé quand la modal s'ouvre, pour que la touche tab se place sur seulement les éléments focusables de la modal

--- javascript/photographe.js
+++ javascript/photographe.js
@@ -4,7 +4,7 @@
 const gallery = document.querySelectorAll(".gallery__sample img"),
 previewBox = document.querySelector(".preview-box"),
 previewImg = previewBox.querySelector("img")
-closeIcon = previewBox.querySelector(".icon"),
+closeIcon = previewBox.querySelector(".imgBox__close"),
 currentImg = previewBox.querySelector(".current-img"),
 totalImg = previewBox.querySelector(".total-img");
 shadow = document.querySelector(".shadow");
@@ -86,5 +86,150 @@ window.onload = () => {  // Une fois la fenêtre chargée
 
 
 
+// ****************** BOUTON CONTACT *************************************
+// const contactBtn = document.querySelector(".contact");
+// const modal = document.querySelector(".modal");
+// const closeBtn = document.querySelector(".modal__head__close");
+
+// contactBtn.onclick = () => {
+//     modal.style.display = "block";
+// }
+
+// closeBtn.onclick = () => {
+    
+//     modal.style.display = "none";
+// }
+
+// ****************** BOUTON SUBMIT **************************************
+const submitBtn = document.querySelector("#submit");
+const prenom = document.querySelector("#prenom");
+const nom = document.querySelector("#nom");
+const email = document.querySelector("#email");
+
+submitBtn.onclick = function(e) {
+    e.preventDefault();
+    console.log(prenom.value);
+    console.log(nom.value);
+    console.log(email.value);
+}
+
+
+// ******************* OUVERTURE FENETRE MODALE **************************
+// Récupérer les composants de type aria-haspopup="dialog" d1 document
+// ainsi que les fenêtres modales liées
+const triggers = document.querySelectorAll('[aria-haspopup="dialog"]');
+const doc = document.querySelector('.js-document');
+// Selection de tous les éléments focalisables de la modale
+const focusableElementsArray = [
+  '[href]',
+  'button:not([disabled])',
+  'input:not([disabled])',
+  'select:not([disabled])',
+  'textarea:not([disabled])',
+  '[tabindex]:not([tabindex="-1"])',
+];
+const keyCodes = {
+  tab: 9,
+  enter: 13,
+  escape: 27,
+};
+// Fonction open qui passe l'attribut aria-hidden à false pour 
+// pouvoir afficher la modale et désactiver le document principal
+const open = function (dialog) {
+  const focusableElements = dialog.querySelectorAll(focusableElementsArray);
+  const firstFocusableElement = focusableElements[1];
+  const lastFocusableElement = focusableElements[focusableElements.length - 1];
+
+  dialog.setAttribute('aria-hidden', false);
+  doc.setAttribute('aria-hidden', true);
+
+  // return si pas d'élément focusable
+  if (!firstFocusableElement) {
+    return;
+  }
+    // Pour que le focus se charge après le lancement de la modale
+    // qui apparaît avec une transition
+  window.setTimeout(() => {
+    firstFocusableElement.focus();
+
+    // trapping focus inside the dialog
+    focusableElements.forEach((focusableElement) => {
+      if (focusableElement.addEventListener) {
+        focusableElement.addEventListener('keydown', (event) => {
+          const tab = event.which === keyCodes.tab;
+
+          if (!tab) {
+            return;
+          }
+
+          if (event.shiftKey) {
+            if (event.target === firstFocusableElement) { // shift + tab
+              event.preventDefault();
+
+              lastFocusableElement.focus();
+            }
+          } else if (event.target === lastFocusableElement) { // tab
+            event.preventDefault();
+
+            firstFocusableElement.focus();
+          }
+        });
+      }
+    });
+  }, 100);
+};
+// Fonction close qui passe l'attribut aria-hidden à true pour 
+// pouvoir fermer la modale et réactiver le document principal
+const close = function (dialog, trigger) {
+  dialog.setAttribute('aria-hidden', true);
+  doc.setAttribute('aria-hidden', false);
+
+  // restoring focus
+  trigger.focus();
+};
+
+triggers.forEach((trigger) => {
+  const dialog = document.getElementById(trigger.getAttribute('aria-controls'));
+  // Récupérer éléments ayant data-dismiss en attribut (boutons de fermeture) et appeler la fonction close
+  const dismissTriggers = dialog.querySelectorAll('[data-dismiss]');
+
+  // Au clic donc du trigger, on envoie la fonction (open)
+  trigger.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    open(dialog);
+  });
+
+  trigger.addEventListener('keydown', (event) => {
+    if (event.which === keyCodes.enter) {
+      event.preventDefault();
+
+      open(dialog);
+    }  
+  });
+
+  // close dialog
+  dialog.addEventListener('keydown', (event) => {
+    if (event.which === keyCodes.escape) {
+      close(dialog, trigger);
+    }      
+  });
+
+  dismissTriggers.forEach((dismissTrigger) => {
+    const dismissDialog = document.getElementById(dismissTrigger.dataset.dismiss);
+
+    dismissTrigger.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      close(dismissDialog, trigger);
+    });
+  });
+
+  window.addEventListener('click', (event) => {
+    if (event.target === dialog) {
+      close(dialog, trigger);
+    }
+  }); 
+});
 
 
